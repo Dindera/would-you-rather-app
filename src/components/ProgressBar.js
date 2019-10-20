14c671d803814b4a3a8004a2bfce1c06c2517373
Chidera Ozoh <Chideraozoh@icloud.com>
2019-10-20 23:11:42 +0100

    Changed title

--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -5,7 +5,7 @@ export const ProgressBar = (props) => {
 
     return (
       <div className='progress-bar'>
-          <div className='progress-filler'  style={{width: `${props.percentage}%`}}></div>
+          <div className='progress-filler'  style={{width: `${props.percentage}%`}}><span style={{fontSize: 10}}>{props.percentage}%</span></div>
       </div>
     )
 }
